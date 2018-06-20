b868e8fcd6718a93d4b8903fe81151d78a634cd5
David Rodríguez <deivid.rodriguez@riseup.net>
2018-06-20 20:26:34 -0300

    Better fix for issue
    The problem was actually that we were setting the `BUNDLE_GEMFILE` env
    variable globally, but this is something that won't happen in decidim's
    executable real usage.

--- decidim-generators/lib/decidim/generators/app_generator.rb
+++ decidim-generators/lib/decidim/generators/app_generator.rb
@@ -112,7 +112,7 @@ module Decidim
           end
         end
 
-        Bundler.with_original_env { run "BUNDLE_GEMFILE=Gemfile bundle install" }
+        Bundler.with_original_env { run "bundle install" }
       end
 
       def tweak_bootsnap
